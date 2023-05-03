43ab36cd5cc7cb059fdb9bd5d1d490fad801fd45
ritaqyang <ritaqyang@gmail.com>
2023-05-02 23:59:37 -0400

    still working on EY only getting one item per page

---
+++ api/beautifulsoup/evrealestate.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('https://api.scrapingdog.com/scrape?api_key=6450247bd9ac1c06d00dbf8f&url=http://montreal.evrealestate.com/inventaire')
+
+
+
+
+
